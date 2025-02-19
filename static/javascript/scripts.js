c81bb93c551d7ea7573dcfa01e88db9d8864d21f
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-02-19 12:57:00 -0300

    Create scripts.js

---
+++ static/javascript/scripts.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    console.log("Chamada de Toggled funcionando!")
+    const navLinks = document.querySelector('#nav-links');
+    navLinks.classList.toggle('active'); /* Adiciona ou remove a classe active*/
+}
