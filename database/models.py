fdbee593f5f954a7b2a8c0589839640e3dcec1f7
Jackson Fidelix <jcksfidelix@gmail.com>
2025-02-26 07:08:43 -0300

    Create Database

--- database/models.py
+++ database/models.py
@@ -4,8 +4,17 @@ db = SQLAlchemy() #criando a instancia do BD
 
 class Register(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
-    product_name = db.Column(db.String(100), nullable=False, unique=True)
+    product_name = db.Column(db.String(100), index=True, nullable=False, unique=True)
     amount = db.Column(db.Integer,  nullable=False)
     average_cost_value = db.Column(db.Float, nullable=False)
     average_sale_value = db.Column(db.Float, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
+
+class Buy(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    id_resgister = db.Column(db.Integer, foreign_key=True, unique=True)
+    product_name = db.Column(db.String(100),index=True, nullable=False, unique=True)
+    cost_value = db.Column(db.Float, nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    buy_date = db.Column(db.DateTime, nullable=False)
+     
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from flask import Flask
-from models import db,Register
+from database.models import db, Register, Buy
 
 
 app = Flask(__name__)
 
+# Configuraçao do banco
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///estoque.db'
+# desativa um recurso do Flask-SQLAlchemy que monitora todas as alterações feitas nos objetos do banco de dados.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializa o SQLAlchemy com este app
+db.init_app(app)
+
 from routes import *
 
 if __name__ == "__main__":
+    # Cria as tabelas dentro do contexto do app
+    with app.app_context():
+        db.create_all()
+        # baixar DB Browser for SQLite (Windows, Mac, Linux) para ver a instancia
     app.run()
