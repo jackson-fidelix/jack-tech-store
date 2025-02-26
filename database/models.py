cd6b8bca45c4b2acae46aaac7f274b9cef17ba92
Jackson Fidelix <116315846+jackson-fidelix@users.noreply.github.com>
2025-02-26 15:37:50 -0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy() #criando a instancia do BD
 
-class Register(db.Model):
+class register(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     product_name = db.Column(db.String(100), index=True, nullable=False, unique=True)
     amount = db.Column(db.Integer,  nullable=False)
@@ -10,11 +10,18 @@ class Register(db.Model):
     average_sale_value = db.Column(db.Float, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
 
-class Buy(db.Model):
+class buy(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
     id_resgister = db.Column(db.Integer, foreign_key=True, unique=True)
     product_name = db.Column(db.String(100),index=True, nullable=False, unique=True)
     cost_value = db.Column(db.Float, nullable=False)
     amount = db.Column(db.Integer, nullable=False)
     buy_date = db.Column(db.DateTime, nullable=False)
-     
+
+class sale(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    id_register = db.Column(db.Integer, foreign_key=True, unique=True)
+    product_name = db.Column(db.String(100), index=True, nullable=False, unique=True)
+    sale_value = db.Column(db.Float, nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    sale_date = db.Column(db.DateTime, nullable=False)
